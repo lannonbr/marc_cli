22a6bf6008c3bc16c3817fc829864f3b4942f985
Benjamin Lannon <lannonbr@clarkson.edu>
2016-11-29 11:41:56 -0500

    Added "Load" command to parser

--- src/parse.rs
+++ src/parse.rs
@@ -1,10 +1,17 @@
 use loader;
 
+fn load_file(filename: &str) {
+    println!("Loading {}", filename);
+    let records = loader::load_records(filename);
+    println!("Size of {}: {}", filename, records.len());
+}
+
 // The main function to parse commands entered in the CLI
 pub fn parse_command(line: &str) {
+    let word_vec: Vec<&str> = line.split(' ').collect();
 
-    //For now, it will try loading a .mrc file only.
-    println!("Loading {}", line);
-    let records = loader::load_records(line);
-    println!("Size of {}: {}", line, records.len());
+    match word_vec[0] {
+        "Load" => load_file(word_vec[1]),
+        _ => println!("Unknown Command."),
+    }    
 }
